28893d5ec826bb6105eadf86760749f853652698
Discovery1701A <klettern.rosine0v@icloud.com>
2024-03-10 19:02:19 +0100

    ich mag Schoki

---
+++ Rezeptbuch/Rezeptbuch/RezeptView.swift
@@ -0,0 +1,89 @@
+//
+//  RezeptView.swift
+//  Rezeptbuch
+//
+//  Created by Anna Rieckmann on 10.03.24.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var recipe: Recipe
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .center, spacing: 10) {
+                Text(recipe.title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+
+                VStack(alignment: .center, spacing: 5) {
+                    Text("Zutaten:")
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+
+                    ForEach(recipe.ingredients, id: \.self) { ingredient in
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 1)
+                            .frame(minHeight: 30)
+                            .overlay(
+                                Text("• \(ingredient)")
+                                    .foregroundColor(.blue)
+                                    .padding(.horizontal, 10)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            )
+                            .padding(.vertical, 5)
+                    }
+                }
+
+                VStack(alignment: .center, spacing: 5) {
+                    Text("Anleitung:")
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+
+                    ForEach(recipe.instructions, id: \.self) { instruction in
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.green, lineWidth: 1)
+                            .frame(minHeight: 30)
+                            .overlay(
+                                Text("\(instruction)")
+                                    .foregroundColor(.green)
+                                    .padding(.horizontal, 10)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            )
+                            .padding(.vertical, 5)
+                    }
+                }
+
+                if let imageName = recipe.image {
+                    Image(imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                        .padding(.top, 10)
+                        .frame(maxWidth: .infinity, maxHeight: 200)
+                }
+            }
+            .padding()
+            .background(Color.white)
+            .cornerRadius(15)
+            .shadow(radius: 5)
+        }
+    }
+}
+
+// Beispiel für die Verwendung
+struct contentView: View {
+    var body: some View {
+        RecipeView(recipe: brownie)
+            .padding()
+            .frame(maxWidth: 400)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        contentView()
+    }
+}
---
+++ Rezeptbuch/Rezeptbuch/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Rezeptbuch
+//
+//  Created by Anna Rieckmann on 10.03.24.
+//
+
+import Foundation
+
+struct Recipe {
+    var title: String
+    var ingredients: [String]
+    var instructions: [String]
+    var image: String? // Pfad zur Bilddatei oder URL
+}
+
+// Beispiel für die Verwendung
+let pastaRecipe = Recipe(
+    title: "Spaghetti Bolognese",
+    ingredients: ["500g Hackfleisch", "1 Zwiebel", "2 Knoblauchzehen", "Tomatensoße", "Spaghetti"],
+    instructions: ["Hackfleisch anbraten", "Zwiebel und Knoblauch hinzufügen", "Tomatensoße dazugeben", "Spaghetti kochen"],
+    image: "spaghetti-mit-schneller-tomatensosse.jpg"
+)
+let brownie = Recipe(title: "Brownie", ingredients: ["250g Schokolade", "250g Butter", "1TL Vanille-Extrakt", "350g Zucker", "6 Eier", "150g Mehl", "200g Schokostücke"], instructions: ["Offen auf 180C° vorheizen","Schokolade und Butter über einem Wasserbad schmelzen", "Eier mit Vanille-Extrakt und Zucker aufschlagen", "abgekühlte Schokoladen-Butter-Masse langsam zu der Eiermasse geben", "erst Mehl und dann die Schokostücke hinzugeben", "Teig in eine Form oder aufs Backblech geben", "30 minuten in den Backoffen"])
+
+//print("Rezepttitel: \(pastaRecipe.title)")
+//print("Zutaten: \(pastaRecipe.ingredients)")
+//print("Anleitung: \(pastaRecipe.instructions)")
+//if let image = pastaRecipe.image {
+//    print("Bild: \(image)")
+//}
