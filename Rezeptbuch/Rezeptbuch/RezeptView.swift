b9e496d303329be050517b72b1ee0a0bcf1c4087
Discovery1701A <klettern.rosine0v@icloud.com>
2024-03-11 10:06:29 +0100

    kästchen schön

--- Rezeptbuch/Rezeptbuch/RezeptView.swift
+++ Rezeptbuch/Rezeptbuch/RezeptView.swift
@@ -11,64 +11,72 @@ struct RecipeView: View {
     var recipe: Recipe
 
     var body: some View {
+        GeometryReader { geometry in
         ScrollView {
-            VStack(alignment: .center, spacing: 10) {
-                Text(recipe.title)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                if let imageName = recipe.image {
-                    Image(imageName)
-                        .resizable()
-                        .scaledToFit()
-                        .cornerRadius(10)
-                        .padding(.top, 10)
-                        .frame(maxWidth: .infinity, maxHeight: 200)
-                }
-                VStack(alignment: .center, spacing: 5) {
-                    Text("Zutaten:")
-                        .font(.headline)
-                        .multilineTextAlignment(.center)
+            
 
-                    ForEach(recipe.ingredients, id: \.self) { ingredient in
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.blue, lineWidth: 1)
-                            .frame(minHeight: 30)
-                            .overlay(
-                                Text("• \(ingredient)")
-                                    .foregroundColor(.blue)
-                                    .padding(.horizontal, 10)
-                                    .fixedSize(horizontal: false, vertical: true)
-                            )
-                            .padding(.vertical, 5)
+                VStack(alignment: .center, spacing: 10) {
+                    Text(recipe.title)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                    
+                    if let imageName = recipe.image {
+                        Image(imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(10)
+                            .padding(.top, 10)
+                            .frame(maxWidth: .infinity, maxHeight: 200)
+                    }
+                    
+                    VStack(alignment: .center, spacing: 5) {
+                        Text("Zutaten:")
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                        
+                        ForEach(recipe.ingredients, id: \.self) { ingredient in
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.blue, lineWidth: 1)
+                                .frame(minHeight: 30)
+                                .overlay(
+                                    Text("• \(ingredient)")
+                                        .foregroundColor(.blue)
+                                        .padding(.horizontal, 10)
+                                        .fixedSize(horizontal: false, vertical: true)
+                                )
+                                .padding(.vertical, 5)
+                        }
+                    }
+                    
+                    VStack(alignment: .center, spacing: 5) {
+                        Text("Anleitung:")
+                            .font(.headline)
+                            .multilineTextAlignment(.center)
+                        
+                        ForEach(recipe.instructions, id: \.self) { instruction in
+                            
+                            Text("\(instruction)")
+                                .foregroundColor(.green)
+                                .padding(.horizontal)
+                                .padding(.vertical,10)
+                                .fixedSize(horizontal: false, vertical: true)
+                                .frame(width : geometry.size.width*0.9)
+                                .background(RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.green, lineWidth: 1))
+                                    
+                                
+                                    
+                            
+                                .padding(.vertical, 5)
+                        }
                     }
                 }
-
-                VStack(alignment: .center, spacing: 5) {
-                    Text("Anleitung:")
-                        .font(.headline)
-                        .multilineTextAlignment(.center)
-
-                    ForEach(recipe.instructions, id: \.self) { instruction in
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.green, lineWidth: 1)
-                            .frame(minHeight: 30)
-                            .overlay(
-                                Text("\(instruction)")
-                                    .foregroundColor(.green)
-                                    .padding(.horizontal, 10)
-                                    .fixedSize(horizontal: false, vertical: true)
-                            )
-                            .padding(.vertical, 5)
-                    }
-                }
-
-               
+                .padding()
+                .background(Color.white)
+                .cornerRadius(15)
+                .shadow(radius: 5)
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(15)
-            .shadow(radius: 5)
         }
     }
 }
@@ -76,7 +84,7 @@ struct RecipeView: View {
 // Beispiel für die Verwendung
 struct contentView: View {
     var body: some View {
-        RecipeView(recipe: pastaRecipe)
+        RecipeView(recipe: brownie)
             .padding()
             .frame(maxWidth: 400)
     }
